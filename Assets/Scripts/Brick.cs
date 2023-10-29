769b92178281755a9d43d102ec49bd2be082a3dc
Magnus Cochems <magnus@cochems.net>
2023-10-29 02:02:15 +0100

    an unproductive day

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -13,7 +13,6 @@ public class Brick : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
         activeBricks--;
         if (activeBricks == 0) {
-            print("All bricks destroyed");
             LoadNextScene();
         }
         Destroy(gameObject);
