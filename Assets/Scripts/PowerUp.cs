299498a44920f044d19a7d194686b5f7f0603225
Magnus Cochems <magnus@cochems.net>
2023-12-18 00:13:23 +0100

    updated some sprites

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,6 +6,7 @@ public class PowerUp {
 
     public PowerUp() {
         Type = (typesOfPowerUps) Random.Range(0, typesOfPowerUps.GetNames(typeof(typesOfPowerUps)).Length);
+        Type = typesOfPowerUps.Heart;
     }
 
     public void activatePowerUp() {
